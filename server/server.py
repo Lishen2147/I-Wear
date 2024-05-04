d30e705143c776a1636e068587f981fdff493ee3
Ryan <ryyu444@gmail.com>
2024-05-04 11:57:52 -0700

    initial server template

---
+++ server/server.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import numpy as np
+import cv2 as cv
+import warnings
+import pickle
+
+app = Flask(__name__)
+CORS(app)
+warnings.filterwarnings("ignore")
+
+# Load the model
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'image' not in request.files:
+        return jsonify({'error': 'No file provided'})
+    image = request.files['image']
+    print(type(image))
+    # prediction = model.predict(image)
+    # return jsonify({'prediction': prediction.tolist()})
