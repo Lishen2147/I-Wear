e0cd1999afcb32b72ea2bed94383d4e59407dfa5
Ryan <ryyu444@gmail.com>
2024-05-04 10:01:45 -0700

    added eslint-prettier

--- i-wear/src/app/page.tsx
+++ i-wear/src/app/page.tsx
@@ -39,6 +39,8 @@ export default function Home() {
         />
       </div>
 
+      <div></div>
+
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
