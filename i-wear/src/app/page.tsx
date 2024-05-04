d0dc6a1aa79534de09fa21f2661e3aff7fd7fe0d
schen7239 <schen7239@gmail.com>
2024-05-04 12:49:53 -0700

    Upload section

--- i-wear/src/app/page.tsx
+++ i-wear/src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
+import Upload from "../components/Upload";
 
 export default function Home() {
   return (
     <main className="bg-black w-screen h-screen">
       <Header />
+      <Upload />
     </main>
   );
 }
